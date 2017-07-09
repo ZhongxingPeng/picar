8d697178940138609ec4fbd11f80dcef72d4c03d
Heechul Yun <heechul@illinois.edu>
2017-07-09 09:02:03 -0500

    debug params

--- params.py
+++ params.py
@@ -6,8 +6,8 @@ from collections import OrderedDict
 
 batch_size = 100
 # save_dir = os.path.abspath('models')
-save_dir = os.path.abspath('../picar/models')
-training_steps = 2000
+save_dir = os.path.abspath('../picar/models-test')
+training_steps = 200
 img_height = 66
 img_width = 200
 img_channels = 3
@@ -26,8 +26,10 @@ if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
 
 epochs = OrderedDict()
-epochs['train'] = [6,7,8,9,10,11,12] # range(0,5)
-epochs['val'] = [13] # range(0,5)
+epochs['train'] = [6] # range(0,5)
+epochs['val'] = [7] # range(0,5)
+# epochs['train'] = [6,7,8,9,10,11,12] # range(0,5)
+# epochs['val'] = [13] # range(0,5)
 # epochs['train'] = [3, 4, 5, 6, 8]
 # epochs['val'] = [1, 2, 7, 9, 10]
 
